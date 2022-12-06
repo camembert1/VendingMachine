fd4da45e221aea6ad6c25637a9d671b7a53bb45a
ige33 <ige33@YJ-BOOK>
2022-12-06 09:04:13 +0900

    INIT : Vending Machine

---
+++ src/com/kh/lyj/Beverage.java
@@ -0,0 +1,38 @@
+package com.kh.lyj;
+
+public class Beverage {
+	private String name;
+	private int price;
+	private int stock;
+	public int itemNum;
+
+	public Beverage() {}
+
+	// setter 메소드
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+
+	// getter 메소드
+	public String getName() {
+		return this.name;
+	}
+
+	public int getPrice() {
+		return this.price;
+	}
+
+	public int getStock() {
+		return this.stock;
+	}
+
+}
